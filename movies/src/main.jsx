8b63fa090e68dd02091a7b189d4d03fa0f90cf79
Kyle <20097726@mail.wit.ie>
2025-10-17 16:27:39 +0100

    Movie Details Page rendering.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/homepage";
+import MovieDetailsPage from './pages/movieDetailsPage'
 
 const sample = {
   adult: false,
@@ -11,6 +12,7 @@ const sample = {
     poster_path: "/iTQHKziZy9pAAY4hHEDCGPaOvFC.jpg",
     backdrop_path: "/d8duYyyC9J5T825Hg7grmaabfxQ.jpg",
   },
+  
   budget: 200000000,
   genres: [
     {
@@ -83,13 +85,28 @@ const sample = {
   vote_count: 9692,
 };
 
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
+
+
 const movies = [sample, sample, sample, sample, sample, sample, sample];
 
 const App = () => {
   return (
-      <HomePage movies={movies} />
-  );
+      <MovieDetailsPage movie={sample} images={images} />
+      );
 };
+
+
+//const App = () => {
+//  return (
+//      <HomePage movies={movies} />
+//  );
+//};
 
 const rootElement = createRoot( document.getElementById("root") )
 rootElement.render(<App />);
