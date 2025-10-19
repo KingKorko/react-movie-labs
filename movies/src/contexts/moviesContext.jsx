862aa6486675b5b059203d1bca33ce499ec36939
Kyle <20097726@mail.wit.ie>
2025-10-19 01:39:17 +0100

    Tagging upcoming movies as must-watch - completed Movie Labs!

--- movies/src/contexts/moviesContext.jsx
+++ movies/src/contexts/moviesContext.jsx
@@ -4,7 +4,9 @@ export const MoviesContext = React.createContext(null);
 
 const MoviesContextProvider = (props) => {
   const [favorites, setFavorites] = useState( [] )
-    const [myReviews, setMyReviews] = useState( {} ) 
+    const [myReviews, setMyReviews] = useState( {} )
+    const [mustWatch, setMustWatch] = useState( [] ) 
+    
 
 
   const addToFavorites = (movie) => {
@@ -18,11 +20,24 @@ const MoviesContextProvider = (props) => {
     setFavorites(newFavorites)
   };
   
+  
   const addReview = (movie, review) => {
     setMyReviews( {...myReviews, [movie.id]: review } )
   };
   console.log(myReviews);
 
+    const addToPlaylist = (movie) => {
+    let newMustWatch = [];
+    if (!mustWatch.includes(movie.id)){
+      newMustWatch = [...mustWatch, movie.id];
+    }
+    else{
+      newMustWatch = [...mustWatch];
+    }
+    setMustWatch(newMustWatch)
+  };
+
+  console.log(mustWatch);
 
   // We will use this function in the next step
   const removeFromFavorites = (movie) => {
@@ -38,6 +53,7 @@ const MoviesContextProvider = (props) => {
         addToFavorites,
         removeFromFavorites,
         addReview,
+        addToPlaylist,
       }}
     >
       {props.children}
